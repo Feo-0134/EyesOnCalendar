352d5bb00d5806cebdf75b78b8c00f34e2c0b1a5
Feo-0134 <zhujuncheng1021@gmail.com>
2019-11-15 14:49:45 +0800

    feat. demo custom dayType

--- models/NewMonth.js
+++ models/NewMonth.js
@@ -6,12 +6,9 @@ var daySchema = new Schema({
   day: Number,
   workType: {
     type: String,
-    enum: ['W', 'PH', 'SL', 'AL', 'H(M)', 'H(A)', 'V', 'T', 'MS',
-      'NS', 'PO', 'PM', 'DV', 'HMSL', 'HMAL', 'HASL', 'HAAL']
   },
   workDay: {
     type: Number,
-    enum: [0, 1, 2, 3, 4, 5]
   }
 })
 
@@ -30,11 +27,15 @@ var personSchema = new Schema({
 })
 
 var monthSchema = new Schema({
+  customDayType: {
+    Type: [],
+    color: [],
+  },
   year: Number,
   month: Number,
   pod: String,
   lockdate: [],
-  people: [personSchema]
+  people: [personSchema],
 })
 
 monthSchema.index({ year: 1, month: 1, pod: 1 }, { unique: true })
